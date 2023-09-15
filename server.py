d2eaf0574c1dc00b5d3142667af1a92cd1fed166
John Doe <johndoe@example.com>
2023-09-14 22:08:22 -0300

    server.py agregado

---
+++ server.py
@@ -0,0 +1,52 @@
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
+import subprocess
+import os
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+upload_dir = "imagenes_subidas"
+os.makedirs(upload_dir, exist_ok=True)
+procces_dir = "imagenes_procesadas"
+os.makedirs(procces_dir, exist_ok=True)
+
+@app.post("/upload/")
+async def upload_image(file: UploadFile, filtro: str, threads: int, parametro: float):
+    try:
+        # Check if the file is an image (you can add more strict checks here)
+        if file.filename.endswith((".jpg", ".jpeg", ".png", ".webp")):
+            # Generate a unique filename
+            file_name = os.path.join(upload_dir, file.filename)
+            with open(file_name, "wb") as image_file:
+                image_file.write(file.file.read())
+
+        
+
+        command = f"./main {filtro} {threads} {parametro} ./imagenes_subidas/{file_name} imagenes_procesadas"
+        try:
+            # Run the command and capture the output
+            time = subprocess.check_output(command, shell=True, text=True)
+            
+            # Print the output
+            print("C++ script output:")
+            print(time)
+
+        except subprocess.CalledProcessError as e:
+            print(f"Error running C++ script: {e}")
+
+
+            return JSONResponse(content={"message": "Image uploaded successfully"}, status_code=200)
+        else:
+            return JSONResponse(content={"message": "Invalid file format. Only images are allowed."}, status_code=400)
+    except Exception as e:
+        return JSONResponse(content={"message": f"An error occurred: {str(e)}"}, status_code=500)
+
+
